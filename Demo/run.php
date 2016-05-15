7b0186627d056e12a3e168274a4320c98dea20a3
slince <taosikai@yeah.net>
2016-05-15 20:41:32 +0800

    调整页面资源采集顺序

--- Demo/run.php
+++ Demo/run.php
@@ -5,6 +5,10 @@ use Slince\Collector\Collector;
 use Slince\Event\Event;
 
 $collector = new Collector(__DIR__ . '/html', 'http://demo.sc.chinaz.com/Files/DownLoad/moban/201604/moban1178/index.html');
+$collector->getDispatcher()->bind(Collector::EVENT_CAPTURE_URL_REPOSITORY, function(Event $event){
+    $repository = $event->getArgument('repository');
+    echo 'Begin Capture ' , $repository->getUrl()->getUrlString(), "\r\n";
+});
 $collector->getDispatcher()->bind(Collector::EVENT_CAPTURED_URL_REPOSITORY, function(Event $event){
     $repository = $event->getArgument('repository');
     echo $repository->getUrl()->getUrlString() . " Captured OK!\r\n";
